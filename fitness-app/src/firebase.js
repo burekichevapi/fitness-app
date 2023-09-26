74f471804bdc06686a1d470f5ea04f494e1197ee
Burek <amer.delic777@protonmail.com>
2023-09-26 18:34:40 -0500

    Delete unneeded comments and files.

--- fitness-app/src/firebase.js
+++ fitness-app/src/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
 
 const firebaseConfig = {
   apiKey: "AIzaSyA1HWaKkjLiFnmPPLnsr9B5lAgg4k3lIro",
