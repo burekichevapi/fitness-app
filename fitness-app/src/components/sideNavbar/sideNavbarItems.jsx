da02f07a4290940cd1767af05b43bd443a0a06d9
Burek <amer.delic777@protonmail.com>
2023-10-28 09:27:12 -0500

    make nav item background transparent

--- fitness-app/src/components/sideNavbar/sideNavbarItems.jsx
+++ fitness-app/src/components/sideNavbar/sideNavbarItems.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const NavbarItems = () =>
-  <ul className="nav flex-column bg-dark mb-0">
+  <ul className="nav flex-column bg-transparent mb-0">
     <li className="nav-item">
       <Link className="nav-link text-dark font-italic bg-light" to="/">Find Exercises</Link>
     </li>
