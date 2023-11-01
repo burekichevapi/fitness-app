02c24de4d2fff642f5586dca8b3f75e4c83d08dc
Burek <amer.delic777@protonmail.com>
2023-11-01 18:54:14 -0500

    typo

--- fitness-app/src/pages/find-exercises.jsx
+++ fitness-app/src/pages/find-exercises.jsx
@@ -61,7 +61,7 @@ const FindExercises = () => {
                   >
                     <option value={1}>1</option>
                     <option value={5}>5</option>
-                    <option value={9}>10</option>
+                    <option value={10}>10</option>
                     <option value={15}>15</option>
                   </select>
                 </div>
