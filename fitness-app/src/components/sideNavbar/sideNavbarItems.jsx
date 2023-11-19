67159385b26ea28a74a3db9e93ac67c01d477e3f
Shaun Shrestha <saurav2220@gmail.com>
2023-11-18 20:36:20 -0600

    Revert change to hide nav bar.

--- fitness-app/src/components/sideNavbar/sideNavbarItems.jsx
+++ fitness-app/src/components/sideNavbar/sideNavbarItems.jsx
@@ -1,40 +1,16 @@
 import { Link } from "react-router-dom";
 
-const NavbarItems = () => (
-  <>
-    <style>
-      {`
-        @media (max-width: 710px) {
-          .hide-on-small-screen {
-            display: none;
-          }
-        }
-      `}
-    </style>
-    <ul className="nav flex-column bg-transparent mb-0">
-      <li className="nav-item">
-        <Link
-          className="nav-link text-dark font-italic bg-light"
-          to="/my-workout"
-        >
-          Favorites
-        </Link>
-      </li>
-      <li className="nav-item hide-on-small-screen">
-        <Link
-          className="nav-link text-dark font-italic bg-light"
-          to="/goals-progress"
-        >
-          My Progress
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link text-dark font-italic bg-light" to="/">
-          Find Exercises
-        </Link>
-      </li>
-    </ul>
-  </>
-);
+const NavbarItems = () =>
+  <ul className="nav flex-column bg-transparent mb-0">
+    <li className="nav-item">
+      <Link className="nav-link text-dark font-italic bg-light" to="/my-workout">Favorites</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link text-dark font-italic bg-light" to="/goals-progress">My Progress</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link text-dark font-italic bg-light" to="/">Find Exercises</Link>
+    </li>
+  </ul>;
 
 export default NavbarItems;
